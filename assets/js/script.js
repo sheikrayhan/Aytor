10de236c663866f53f068456bfc24a04ca58f017
sheikrayhan <sheikrayhan359@gmail.com>
2022-12-24 19:12:55 +0600

    banner_add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 $(function () {
    'use strict';
+
+   $('.banner_slider').slick({
+      arrows: false,
+      dots: true,
+      dotsClass: 'banner_slider_dots ',
+      autoplay: true,
+      autoplaySpeed: 2000
+   });
 })
