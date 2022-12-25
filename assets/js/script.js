88ee9119ad7e55047ea5e7c0786e87692edc006b
sheikrayhan <sheikrayhan359@gmail.com>
2022-12-25 06:16:09 +0600

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,21 @@
 $(function () {
    'use strict';
 
+   // Banner Slider Activate
    $('.banner_slider').slick({
       arrows: false,
       dots: true,
       dotsClass: 'banner_slider_dots ',
       autoplay: true,
       autoplaySpeed: 2000
+   });
+
+   // New Arrival Product slider Activate
+   $('.Product_slider').slick({
+      arrows: false,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
    });
 })
